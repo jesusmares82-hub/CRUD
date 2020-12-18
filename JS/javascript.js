2f189567962c2d285a9bfa1b52db3519227554cc
Jesus <jesusmares82@gmail.com>
2020-12-18 16:26:51 -0600

    marcia test 2

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,62 +1,73 @@
 const cars = [
   {
     id: 1,
-    carBrand: "Erik",
-    model: 29,
-    color: "#7143b6",
+    pic: "./IMG/car.jpeg",
+    carBrand: "Toyota",
+    model: "Avanza",
+    color: "#7143B6",
     age: 2018,
     price: 250000,
   },
   {
     id: 2,
-    carBrand: "Robe",
-    model: 29,
-    color: "#7143b6",
+    pic: "./IMG/car.jpeg",
+    carBrand: "Nissan",
+    model: "March",
+    color: "#FAFA10",
     age: 2018,
     price: 250000,
   },
   {
     id: 3,
-    carBrand: "Alex",
-    model: 29,
-    color: "#7143b6",
+    pic: "./IMG/car.jpeg",
+    carBrand: "Volkswagen",
+    model: "Sedan",
+    color: "#00FCDB",
     age: 2018,
     price: 250000,
   },
   {
     id: 4,
-    carBrand: "Mau",
-    model: 29,
-    color: "#7143b6",
+    pic: "./IMG/car.jpeg",
+    carBrand: "Suzuki",
+    model: "Swift",
+    color: "#002AFC",
     age: 2018,
     price: 250000,
   },
   {
     id: 5,
-    carBrand: "Jhon",
-    model: 29,
-    color: "#7143b6",
+    pic: "./IMG/car.jpeg",
+    carBrand: "General Motors",
+    model: "Tracker",
+    color: "#F100FC",
     age: 2018,
     price: 250000,
   },
 ];
 
 function readCars() {
-  // 1. Obtengo el elemento html en el que quiero poner los usuarios
-  // 2. Genero el html de los usuarios
-  // 3. Pongo el html en el elemento obtenido
   const container = document.getElementById("container-users");
   let html = "";
   cars.forEach((car) => {
     html += `<tr>
+                    <td><img src="${car.pic}" alt="Car"  height=40 width=60></img></td>                
                     <td class="text-white">${car.carBrand}</td>
                     <td class="text-white">${car.model}</td>
-                    <td class="text-white">${car.color}</td>
+                    <td  style="background-color:${car.color}"></td>
                     <td class="text-white">${car.age}</td>
                     <td class="text-white">${car.price}</td>
                     <td>
-                        <button onclick="deleteCar(${car.id})" class="btn btn-danger">
-                            Eliminar
+                    <button data-bs-toggle="modal"
+        data-bs-target="#staticBackdrop" onclick="addCar(event)" data-bs-whatever="Agregar32 Carro"  class="btn btn-success me-md-2">
+                            Agregar
+                        </button>
+                        <button onclick="deleteCar(${car.id})" class="btn btn-danger me-md-2">
+                            Delete
+                        </button>
+                        <button data-bs-toggle="modal"
+        data-bs-target="#staticBackdrop" onclick="updateCar(event,${car.id})" data-bs-whatever="Actualizar32 Carro"  class="btn btn-success">
+                            Update
                         </button>
                     </td>
                 </tr>`;
@@ -65,26 +76,27 @@ function readCars() {
 }
 
 function deleteCar(id) {
-  // Como elimino un valor de un arreglo? 1. pop -> shift -> splice
-  // necesito el índice -> ¿cómo obtengo el índice del elemento si lo que yo recibo es el id? -> findIndex
   const index = cars.findIndex((car) => car.id == id);
   cars.splice(index, 1);
   readCars();
 }
 
-function addCar() {
-  // obtner el valor del input
-  // agregar el usuario al arreglo
-  // imprimo nuevamente los usuarios
+function addCar(event) {
+  //var modalTitle = exampleModal.querySelector(".modal-title");
+  //modalTitle.textContent = "Agregar Carro";
+  //document.getElementById("form-user").reset();
 
+  alert("Entre a Agregar carro ");
+  const pic = "./IMG/car.jpeg";
+  const color = document.querySelector("[type='color']").value;
   const carBrand = document.getElementById("inputCarBrand").value;
   const model = document.getElementById("inputModel").value;
-  const color = document.getElementById("inputColor").value;
   const age = document.getElementById("inputYear").value;
   const price = document.getElementById("inputPrice").value;
   const id = cars[cars.length - 1].id + 1;
 
   const newCar = {
+    pic,
     id,
     carBrand,
     model,
@@ -97,6 +109,43 @@ function addCar() {
 
   // limpiamos el formulario
   document.getElementById("form-user").reset();
+  event.preventDefault();
 }
+
+function updateCar(event, id) {
+  //exampleModal.addEventListener('show.bs.modal', function (event) {
+  //alert("Actualizar el id; " + id);
+  event.preventDefault();
+  var modalTitle = exampleModal.querySelector(".modal-title");
+  modalTitle.textContent = "Actualizar Carro";
+
+  alert("Actualizar el id " + id);
+  document.getElementById("inputCarBrand").value = cars[id - 1].carBrand;
+  document.getElementById("inputModel").value = cars[id - 1].model;
+  document.querySelector("[type='color']").value = cars[id - 1].color;
+  document.getElementById("inputYear").value = cars[id - 1].age;
+  document.getElementById("inputPrice").value = cars[id - 1].price;
+
+  cars[id - 1].carBrand = document.getElementById("inputCarBrand").value;
+
+  readCars();
+}
+
+var exampleModal = document.getElementById("staticBackdrop");
+exampleModal.addEventListener("show.bs.modal", function (event) {
+  // Button that triggered the modal
+  var button = event.relatedTarget;
+  // Extract info from data-bs-* attributes
+  var recipient = button.getAttribute("data-bs-whatever");
+  // If necessary, you could initiate an AJAX request here
+  // and then do the updating in a callback.
+  //
+  // Update the modal's content.
+  var modalTitle = exampleModal.querySelector(".modal-title");
+  var modalBodyInput = exampleModal.querySelector(".modal-body input");
+
+  modalTitle.textContent = "New message to " + recipient;
+  modalBodyInput.value = recipient;
+});
 
 readCars();
